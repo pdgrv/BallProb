6246fa058a86ed0d3e8335bfcc4629a0e2befdca
Maxim Podgorov <mapodgorov@gmail.com>
2020-09-10 16:57:20 +0700

    Add camera mover and TMP

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _smoothSpeed = 1f;
+
+    private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = _target.position - transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        if (_target == null)
+            return;
+
+        Vector3 desiredPosition = _target.position - _offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, _smoothSpeed);
+    }
+}
