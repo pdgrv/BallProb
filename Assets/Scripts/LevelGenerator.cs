0bdc30c77f20918169d7eed8f4044b809b4a08b9
Maxim Podgorov <mapodgorov@gmail.com>
2020-09-12 03:22:34 +0700

    camera-based levelGeneration

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -7,14 +7,22 @@ public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] private List<GridObject> _templates;
     [SerializeField] private Transform _player;
-    [SerializeField] private float _viewRadius;
     [SerializeField] private float _cellSize;
 
     private HashSet<Vector3Int> _collisionsMatrix = new HashSet<Vector3Int>();
+    private List<GridObject> _pool = new List<GridObject>();
+
+    private Camera _camera;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
 
     private void Update()
     {
-        FillLine(_player.position, _viewRadius);
+        FillLine(_player.position, _camera.ViewportToWorldPoint(new Vector3(1f, 1f, -_camera.transform.position.z * 2)).x);
+        DestroyObjectAbroadScreen(_pool);
     }
 
     private void FillLine(Vector3 center, float viewRadius)
@@ -22,7 +30,7 @@ public class LevelGenerator : MonoBehaviour
         var cellCountOnAxis = (int)(viewRadius / _cellSize);
         var fillAreaCenter = WorldToGridPosition(center);
 
-        for (int x = -cellCountOnAxis; x <= cellCountOnAxis; x++)
+        for (int x = 0; x <= cellCountOnAxis; x++)
         {
             TryCreateOnLayer(GridLayer.Ground, fillAreaCenter + new Vector3Int(x, 0, 0));
             TryCreateOnLayer(GridLayer.OnGround, fillAreaCenter + new Vector3Int(x, 0, 0));
@@ -43,7 +51,8 @@ public class LevelGenerator : MonoBehaviour
 
         var position = GridToWorldPosition(gridPosition);
 
-        Instantiate(template, position, template.transform.rotation, transform);
+        GridObject createdObject = Instantiate(template, position, template.transform.rotation, transform);
+        _pool.Add(createdObject);
     }
 
     private GridObject GetRandomTemplate(GridLayer layer)
@@ -57,6 +66,26 @@ public class LevelGenerator : MonoBehaviour
         }
 
         return null;
+    }
+
+    private void DestroyObjectAbroadScreen(List<GridObject> objectPool)
+    {
+        Vector3 disablePoint = _camera.ViewportToWorldPoint(new Vector3(0f, 0f, -_camera.transform.position.z));
+
+        foreach (GridObject item in objectPool)
+        {
+            if (item == null)
+            {
+                objectPool.Remove(item);
+                return;
+            }
+
+            if (item.transform.position.x < disablePoint.x)
+            {
+                _collisionsMatrix.Remove(WorldToGridPosition(item.transform.position));
+                Destroy(item.gameObject);
+            }
+        }
     }
 
     private Vector3 GridToWorldPosition(Vector3Int gridPosition)
