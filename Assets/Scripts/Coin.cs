48f9cd9c47110d74a27782d3d04d279311274e3e
Maxim Podgorov <mapodgorov@gmail.com>
2020-09-11 17:57:25 +0700

    Coin rotate and small fix

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float _rotationSpeed;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Rotate(Vector3.back, _rotationSpeed * Time.deltaTime);
     }
 }
